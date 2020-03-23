79c1a9d867b889924550c54ec3a52750cc7d9cd6
sajad1993gh <sajad1993gh@gmail.com>
2020-03-22 22:00:56 -0400

    add homepage

--- public/assets/js/front.js
+++ public/assets/js/front.js
@@ -1,4 +1,12 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+  
 $(function() {
 
 
